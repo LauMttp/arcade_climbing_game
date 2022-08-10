96cf05f034d5463c5ab988c9b52e9288de992084
LauMttp <laurent.mouttapa@gmail.com>
2022-08-10 11:23:54 +0200

    added class Game with related method + DOM document

--- game.js
+++ game.js
@@ -1,26 +1,65 @@
 class Game {
   constructor() {
-    this.combinationsSequence = null;
+    this.combinationsSequence = [];
+    this.checkingArr = [];
     this.score = 0;
   }
 
   createRandomSequence() {
-    // créer la séquence de combinaisons aléatoire !
+    // here we create randomly 50 arrays that we push inside our combinationsSequence
+    const arrows = ["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"];
+    for (let i = 0; i < 50; i++) {
+      const clone = [...arrows];
+      const tempArr = [];
+      const max = Math.ceil(Math.random() * 4);
+      for (let ii = 0; ii < max; ii++) {
+        const a = Math.floor(Math.random() * clone.length);
+        tempArr.push(clone[a]);
+        clone.splice(a, 1);
+      }
+      this.combinationsSequence.push(tempArr);
+    }
   }
 
-  displayCombinations() {
-
+  displayCombinations(elements) {
+    for (let i = 0; i < 6; i++) {
+      const target = this.combinationsSequence[i];
+      let combo = "";
+      if (target.includes("ArrowRight")) {
+        combo += "➡️";
+      }
+      if (target.includes("ArrowLeft")) {
+        combo += "⬅️";
+      }
+      if (target.includes("ArrowUp")) {
+        combo += "⬆️";
+      }
+      if (target.includes("ArrowDown")) {
+        combo += "⬇️";
+      }
+      // we override previous textContent
+      elements[i].textContent = combo;
+    }
   }
 
   checkcombination() {
-
+    // if empty +1, else +0 --> empty the checkingArr
   }
 
   increaseScore() {
-    // updater le score
+    this.score += 5;
+    scoreElement.innerHTML = this.score;
   }
 
   generateNewCombination() {
-    //générer une nouvelle combinaison à chaque combinaison bien exécutée
+    // here we create One Array that we push inside our combinationsSequence
+    const arrows = ["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"];
+    const tempArr = [];
+    const max = Math.floor(Math.random() * 4);
+    for (let i = 0; i < max; i++) {
+      const a = Math.floor(Math.random() * 4);
+      tempArr.push(arrows[a]);
+    }
+    this.combinationsSequence.push(tempArr);
   }
 }
--- index.js
+++ index.js
@@ -1,15 +1,117 @@
-const startBtn = document.querySelector("#start");
-const score = document.querySelector("#score span");
-const board = document.querySelector("#board");
-const display = document.querySelector("#display");
+const startButton = document.querySelector("#start");
+const scoreElement = document.querySelector("#score span");
+const boardElement = document.querySelector("#board");
+const gridElement = document.querySelector("#display");
 const grid = [];
+const board = [];
 let counter = 0;
+let myGame;
 
-/* for (let i = 0; i < 100; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  board.append(cell);
-  grid.push(cell);
+/*function createBoard() {
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    boardElement.append(cell);
+    board.push(cell);
+  }
+}*/
+
+const pressedKeys = {
+  ArrowDown: false,
+  ArrowUp: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
+
+function createGrid() {
+  for (let i = 0; i < 6; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    gridElement.append(cell);
+    grid.push(cell);
+  }
 }
 
-document.addEventListener(KeyboardEvent)*/
+function startNewGame() {
+  gridElement.innerHTML = "";
+  myGame = new Game();
+  myGame.createRandomSequence();
+  //createBoard();
+  createGrid();
+  myGame.displayCombinations(grid);
+  console.log(myGame.combinationsSequence);
+
+  document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        pressedKeys.ArrowUp = true;
+        break;
+      case "ArrowDown":
+        pressedKeys.ArrowDown = true;
+        break;
+      case "ArrowLeft":
+        pressedKeys.ArrowLeft = true;
+        break;
+      case "ArrowRight":
+        pressedKeys.ArrowRight = true;
+        break;
+    }
+
+    if (checkPressedKeys()) {
+      console.log("I just pressed: ", event.key);
+
+      myGame.combinationsSequence.splice(0, 1);
+      myGame.displayCombinations(grid);
+    }
+  });
+  document.addEventListener("keyup", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        pressedKeys.ArrowUp = false;
+        break;
+      case "ArrowDown":
+        pressedKeys.ArrowDown = false;
+        break;
+      case "ArrowLeft":
+        pressedKeys.ArrowLeft = false;
+        break;
+      case "ArrowRight":
+        pressedKeys.ArrowRight = false;
+        break;
+    }
+    //checkPressedKeys()
+  });
+}
+function checkPressedKeys() {
+  const tempPressedKeys = [];
+  for (const key in pressedKeys) {
+    if (pressedKeys[key]) {
+      tempPressedKeys.push(key);
+    }
+  }
+  // Verify that all the tenpPressedKeys exist in the combination
+  // If all the keys are correct: go to next combination with points
+  // If missing a key, do nothing
+  // If a pressedKey is wrong = Go to next combination without points
+
+  console.log(
+    "Combinaison ",
+    myGame.combinationsSequence[0],
+    "pressed keys ",
+    tempPressedKeys
+  );
+
+  const lengthsAreSame =
+    myGame.combinationsSequence[0].length === tempPressedKeys.length;
+  console.log("correct count?", lengthsAreSame);
+  if (!lengthsAreSame) {
+    return false;
+  }
+  const areAllKeysPressed = myGame.combinationsSequence[0].every((key) =>
+    tempPressedKeys.includes(key)
+  );
+  console.log("all pressed?", areAllKeysPressed);
+  return areAllKeysPressed;
+}
+
+startButton.addEventListener("click", startNewGame);
